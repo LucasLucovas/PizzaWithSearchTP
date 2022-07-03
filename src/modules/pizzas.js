07eda98c3c68bd14245ec0f862a410e020313dcd
Lucas Lucovas <lucaslucovas@gmail.com>
2022-07-03 03:09:28 -0300

    cambiando ruta imagenes

--- src/modules/pizzas.js
+++ src/modules/pizzas.js
@@ -2,42 +2,42 @@ const Pizzas =[
 
     {
         id: '1',
-        imagen :"/src/img/pizza-margarita.jpg",
+        imagen :"./src/img/pizza-margarita.jpg",
         nombre: 'Pizza margarita' ,
         ingredientes:["queso"," salsa"," aceite de oliva"," albahaca"],
         precio: "$500"
     },
     {
         id: '2',
-        imagen :"/src/img/pizzaRucula.jpg",
+        imagen :"./src/img/pizzaRucula.jpg",
         nombre: 'Pizza de rucula' ,
         ingredientes:["queso"," salsa"," jamon crudo"," rucula"],
         precio: "$1100"
     },
     {
         id: '3',
-        imagen :"/src/img/polloysalsaBBQ.jpg",
+        imagen :"./src/img/polloysalsaBBQ.jpg",
         nombre: 'Pizza de pollo y salsa BBQ' ,
         ingredientes:["queso"," salsa"," salsa BBQ"," pollo"],
         precio: "$1000"
     },
     {
         id: '4',
-        imagen :"/src/img/matambre-a-la-pizza.jpg",
+        imagen :"./src/img/matambre-a-la-pizza.jpg",
         nombre: 'Matambre a la pizza' ,
         ingredientes:["queso"," queso azul"," matambre"," salsa"],
         precio: "$1500"
     },
     {
         id: '5',
-        imagen :"/src/img/pizzaVeggie.jpg",
+        imagen :"./src/img/pizzaVeggie.jpg",
         nombre: 'Pizza Veggie' ,
         ingredientes:["queso"," salsa"," cebolla"," pimiemtos"," champiñones"],
         precio: "$900"
     },
     {
         id: '6',
-        imagen :"/src/img/pizza-hawaiana.jpg",
+        imagen :"./src/img/pizza-hawaiana.jpg",
         nombre: 'Pizza Hawaiana' ,
         ingredientes:["queso"," salsa"," Anana"," jamon"],
         precio: "$950"
